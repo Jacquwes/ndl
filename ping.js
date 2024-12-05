feb73b9dfb92b2eb7dff73387ac7573bcd4013b4
Maskady <florent.tout@gmail.com>
2024-12-05 20:53:38 +0100

    add ping functionality and character replacement handling

--- ping.js
+++ ping.js
@@ -1,44 +1,49 @@
 
 let buffer = [];
-let previousValue = '';
+let previousString = '';
+let id = 0;
 
-async function packetLossLoop() {
-    // Vérification de la présence d'un élément dans le buffer
+async function packetLossLoop(id_) {
     if(buffer.length > 0) {
-        // Récupération du premier élément du buffer
-        let packet = buffer.shift();
-        // Attente du temps indiqué dans le packet
+        while(buffer[0].id !== id_) {
+            await new Promise(resolve => setTimeout(resolve, 10));
+        }
+        let packet = buffer[0];
         setTimeout(() => {
             let input = document.querySelector('input');
             let value = input.value;
             input.value = value.substring(0, packet.indice) + packet.char + value.substring(packet.indice + 1);
+            buffer = buffer.filter(packet => packet.id !== id_);
         }, packet.time);
-        // Remplacement du caractère à l'indice indiqué
     }
 }
 
 function addPing(e) {
-    // if (Math.floor(Math.random() * 10) !== 0){
-    //     // Récupération du dernier caractère
-    //     return;
-    // }
-    let newchar = e.key;
-    // Déduction de l'indice par le curseur
+    let newChar = e.key;
+    // Vérification que le caractère est un caractère alphanumérique
+    if(!newChar.match(/^.$/i)) {
+        return; 
+    }
     let indice = e.target.selectionStart;
+    console.log("indice avant: " + indice);
+    for(let i = 0; i < buffer.length; i++) {
+        if(buffer[i].indice <= indice) {
+            indice++;
+        }
+    }
+    console.log("indice après: " + indice);
     console.log("pass");
     if(newChar === '') {
         return;
     }
     // Ajout de l'indice, du caractère et d'un nombre en millisecondes < 1000
     buffer.push({
+        id: id++,
         indice: indice,
         char: newChar,
-        time: 500
+        time: Math.floor(Math.random() * 300)
     });
     // Retire le caractère lu de l'input
-    console.log("initialValue", initialValue);
-    document.querySelector('input').value = initialValue;
     e.preventDefault();
-    Promise.resolve().then(packetLossLoop);
+    Promise.resolve().then(() => packetLossLoop(id-1));
 }
-// document.querySelector('input').addEventListener('keypress', replaceChar);
