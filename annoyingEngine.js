5e5ddd27c789317fc41a776b7b6b6eb676439713
Paul Hariel <paul.hariel@outlook.com>
2024-12-05 21:15:35 +0100

    push

--- annoyingEngine.js
+++ annoyingEngine.js
@@ -2,28 +2,40 @@ let annoyances = [
   {
     name: "ping",
     enabled: false,
-    execute: (duration) => console.log("ping"),
+    execute: (duration) => enablePing(duration),
   },
   {
     name: "missclick",
     enabled: false,
-    execute: (duration) => console.log("missclick"),
+    execute: (duration) => enableMissclick(duration),
   },
 ];
 
 function annoyingEngine() {
-  const timeUntilNextAnnoyance = Math.random() * 10000;
+  const timeUntilNextAnnoyance = Math.random() * 1000;
   const randomAnnoyance =
     annoyances[Math.floor(Math.random() * annoyances.length)];
-  const annoyanceDuration = Math.random() * 1000;
+  const annoyanceDuration = Math.random() * 2000;
 
   setTimeout(() => {
+    // Skip if the annoyance is already enabled
+    if (randomAnnoyance.enabled) {
+      return annoyingEngine();
+    }
+
     console.log(
       `Annoying user with ${randomAnnoyance.name} for ${annoyanceDuration}ms`
     );
 
+    //  Enable the annoyance
     randomAnnoyance.enabled = true;
     randomAnnoyance.execute(annoyanceDuration);
+
+    // Disable the annoyance after the duration
+    setTimeout(() => {
+      console.log(`Disabling ${randomAnnoyance.name}`);
+      randomAnnoyance.enabled = false;
+    }, annoyanceDuration);
 
     annoyingEngine();
   }, timeUntilNextAnnoyance);
--- ping.js
+++ ping.js
@@ -1,49 +1,58 @@
+function enablePing(duration) {
+  document.getElementById("impossibleInput").addEventListener("keydown", addPing);
+  setTimeout(() => {
+    document.getElementById("impossibleInput").removeEventListener("keydown", addPing);
+  }, duration);
+}
 
 let buffer = [];
-let previousString = '';
+let previousString = "";
 let id = 0;
 
 async function packetLossLoop(id_) {
-    if(buffer.length > 0) {
-        while(buffer[0].id !== id_) {
-            await new Promise(resolve => setTimeout(resolve, 10));
-        }
-        let packet = buffer[0];
-        setTimeout(() => {
-            let input = document.querySelector('input');
-            let value = input.value;
-            input.value = value.substring(0, packet.indice) + packet.char + value.substring(packet.indice + 1);
-            buffer = buffer.filter(packet => packet.id !== id_);
-        }, packet.time);
+  if (buffer.length > 0) {
+    while (buffer[0].id !== id_) {
+      await new Promise((resolve) => setTimeout(resolve, 10));
     }
+    let packet = buffer[0];
+    setTimeout(() => {
+      let input = document.getElementById("impossibleInput");
+      let value = input.value;
+      input.value =
+        value.substring(0, packet.indice) +
+        packet.char +
+        value.substring(packet.indice + 1);
+      buffer = buffer.filter((packet) => packet.id !== id_);
+    }, packet.time);
+  }
 }
 
 function addPing(e) {
-    let newChar = e.key;
-    // Vérification que le caractère est un caractère alphanumérique
-    if(!newChar.match(/^.$/i)) {
-        return; 
+  let newChar = e.key;
+  // Vérification que le caractère est un caractère alphanumérique
+  if (!newChar.match(/^.$/i)) {
+    return;
+  }
+  let indice = e.target.selectionStart;
+  console.log("indice avant: " + indice);
+  for (let i = 0; i < buffer.length; i++) {
+    if (buffer[i].indice <= indice) {
+      indice++;
     }
-    let indice = e.target.selectionStart;
-    console.log("indice avant: " + indice);
-    for(let i = 0; i < buffer.length; i++) {
-        if(buffer[i].indice <= indice) {
-            indice++;
-        }
-    }
-    console.log("indice après: " + indice);
-    console.log("pass");
-    if(newChar === '') {
-        return;
-    }
-    // Ajout de l'indice, du caractère et d'un nombre en millisecondes < 1000
-    buffer.push({
-        id: id++,
-        indice: indice,
-        char: newChar,
-        time: Math.floor(Math.random() * 300)
-    });
-    // Retire le caractère lu de l'input
-    e.preventDefault();
-    Promise.resolve().then(() => packetLossLoop(id-1));
+  }
+  console.log("indice après: " + indice);
+  console.log("pass");
+  if (newChar === "") {
+    return;
+  }
+  // Ajout de l'indice, du caractère et d'un nombre en millisecondes < 1000
+  buffer.push({
+    id: id++,
+    indice: indice,
+    char: newChar,
+    time: Math.floor(Math.random() * 300),
+  });
+  // Retire le caractère lu de l'input
+  e.preventDefault();
+  Promise.resolve().then(() => packetLossLoop(id - 1));
 }
